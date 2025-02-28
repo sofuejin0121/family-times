12cb7ca9bc7e454340c940dcb07fde622fc32b90
Jin Sofue <jin01213921@gmail.com>
2025-03-01 00:34:19 +0900

    vite-plugin-pwaによるPWAサポートの追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     netlifyPlugin(),
+    VitePWA()
   ],
   resolve: {
     alias: {
